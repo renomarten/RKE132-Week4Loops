470954aa6f1509336ba93e7be64b9894813160f8
Reno. <reno.marten.proosa@tktk.ee>
2024-10-02 21:15:20 +0300

    add Week4Loops code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,16 @@
+﻿
+//FOR-LOOP
+//DRY-principle - Do Not Repeat Yourself
+
+int i = 0;
+
+for(i = 0; i < 5; i++) //i++ = i+i
+{
+    Console.WriteLine("We don't need no education.");
+}
+
+Console.WriteLine("Have a nice day.");
+
+
+
+//Console.WriteLine("We don't need no education.");
